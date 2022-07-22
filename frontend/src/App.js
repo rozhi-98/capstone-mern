881c5f503aea686ffa2e33729dff7109c046a34d
rozh bahman tahir <r.bahman98@gmail.com>
2022-07-22 15:39:12 +0300

    create-react-capstone-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">capstone</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
